282812d38f68de96a864107158e93f5534b420f2
Felipe Gomes <felipesgb2310@gmail.com>
2026-03-20 22:14:50 -0300

    style: estilização da pagina-login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,31 +1,40 @@
 import LogoAkaer from "../assets/LogoAkaer.png";
+import AkaerEscrita from "../assets/AkaerEscrita.png";
+
 function Login() {
   return (
     <>
-      <div className="flex">
-        <div className="flex flex-col">
+      <div className="flex min-h-screen">
+        <div className="w-1/2 flex flex-col items-center justify-center">
           <h1 className="font-bold text-[64px]">BEM-VINDO</h1>
-          <p className="font-bold italic text-[20px] text-[#78787A] mb-28">
+          <p className="font-bold italic text-[20px] text-[#78787A] mb-35">
             Insira seu dados para efetuar o login
           </p>
-          <form className="flex flex-col">
-            <label>E-mail:</label>
-            <input type="text" placeholder="Insira seu Email" className="" />
-            <label>Senha:</label>
+          <form className="flex flex-col w-[500px]">
+            <label className="text-[20px]">E-mail:</label>
+            <input
+              type="text"
+              placeholder="Insira seu Email"
+              className="text-[20px] border rounded-full py-3 pl-4 mb-5 font-light italic"
+            />
+            <label className="text-[20px]">Senha:</label>
             <input
               type="password"
               placeholder="Insira sua Senha"
-              className=""
+              className="text-[20px] border rounded-full py-3 pl-4 mb-10 font-light italic"
             />
             <button
               type="submit"
-              className="bg-[#74213D] rounded-full font-bold text-[32px] text-white "
+              className="bg-[#74213D] rounded-full font-bold text-[32px] text-white"
             >
               Enviar
             </button>
           </form>
         </div>
-        <img src={LogoAkaer} alt="" />
+        <div className="w-1/2 flex flex-col items-center justify-center gap-10 bg-[#C8C8C8]">
+          <img src={LogoAkaer} alt="Logo da Akaer" className="" />
+          <img src={AkaerEscrita} alt="Akaer" />
+        </div>
       </div>
     </>
   );
