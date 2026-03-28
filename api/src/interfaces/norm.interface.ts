e96f57353469e307899d9c2b32ed935b0dd6a764
Hugo leo <hugoleonardo040906@gmail.com>
2026-03-28 11:28:35 -0300

    fix:arrumando bugs do interface

--- api/src/interfaces/norm.interface.ts
+++ api/src/interfaces/norm.interface.ts
@@ -4,7 +4,7 @@ export default interface INorm{
     getNorms():Promise<Array<ResponseNorm>>
     createNorm(norma:CreateNormDTO):Promise<ResponseNorm>
     updateNorm(norma:UpdateNormDTO):Promise<ResponseNorm>
-    deleteNorm(id:string,norm_codigo:string):Promise<ResponseNorm>
+    deleteNorm(id:number):Promise<ResponseNorm>
     getHistocNorms(id_user:number):Promise<Array<ResponseNorm>>
     saveNormsInHistoric(id_norm:number,id_user:number):Promise<void>
 
